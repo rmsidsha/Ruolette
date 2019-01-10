89c3a926fd36561af4931b6c2c8e5ceff617f57a
Administrator <Administrator@bon211-9>
2019-01-10 17:34:07 +0900

    Plus Start button and Create Roulette Box

--- src/nanaitabeRur/Roulette.java
+++ src/nanaitabeRur/Roulette.java
@@ -9,38 +9,56 @@ import javax.swing.border.*;
 
 import java.awt.event.*;
 
-class ruolette extends JPanel{
+class RouletteP extends JPanel{
 	
-	public ruolette() {
-		JPanel ruoletteP = new JPanel();   //Size of ruolette
-		ruoletteP.setSize(500, 500);
-		JLabel ruoletteLabel = new JLabel("Ruolette", JLabel.CENTER);
+	public RouletteP() {
+		JPanel rouletteP = new JPanel();   //Size of roulette
+		rouletteP.setSize(500, 500);
+		JLabel rouletteLabel = new JLabel("Roulette", JLabel.CENTER);
 		/*ImageIcon ruoletteImg = new ImageIcon("path of roulettimg");  //ruolette Image
-		ruoletteLabel.setIcon(ruoleteImg);*/
-		ruoletteLabel.setOpaque(true);
-		ruoletteLabel.setBackground(Color.BLUE);
-		ruoletteP.add(ruoletteLabel);
+		rouletteLabel.setIcon(ruoleteImg);*/
+		rouletteLabel.setOpaque(true);
+		rouletteLabel.setForeground(Color.BLUE);
+		rouletteLabel.setBackground(Color.BLUE);
+		rouletteP.add(rouletteLabel);
+		add(rouletteP);
 		setVisible(true);
 	}
 }
 
+class ButtonP extends JPanel{
+	public ButtonP() {
+	JPanel buttonP = new JPanel();
+	buttonP.setSize(500, 478);
+	
+	JButton startB = new JButton("·ê·¿ START");
+	startB.setSize(200, 200);
+	startB.setForeground(Color.GREEN);
+	buttonP.add(startB);
+	add(buttonP);
+	setVisible(true);
+	}
+}
 public class Roulette extends JFrame{
 	private static final int OVARALL_SIZE_WHITDH = 550;
 	private static final int OVARALL_SIZE_HEIGTH = 978;
 	
 	public Roulette() {
+		RouletteP rp = new RouletteP();
+		ButtonP bp = new ButtonP();
 		setSize(OVARALL_SIZE_WHITDH ,OVARALL_SIZE_HEIGTH );  //Size of screen
 		JPanel viewP = new JPanel();
+		viewP.setLayout(new BorderLayout());
 		viewP.setSize(OVARALL_SIZE_WHITDH, OVARALL_SIZE_HEIGTH);
-		viewP.add(new ruolette(), BorderLayout.NORTH);
+		viewP.add(rp, BorderLayout.NORTH);
+		viewP.add(bp, BorderLayout.SOUTH);
 		add(viewP);
 		setVisible(true);
-		
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Roulette ruo = new Roulette();
+		Roulette rou = new Roulette();
 	
 	}
 
