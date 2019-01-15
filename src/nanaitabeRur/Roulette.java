d63617efb6bc26ecfa2923db44bdf2b992cc5f5d
rmsidsha <43736727+rmsidsha@users.noreply.github.com>
2019-01-15 16:26:55 +0900

    부드럽게 돌아가는 룰렛
    렉을 을 없애고 부드럽게 돌아가게 만듦

--- src/nanaitabeRur/Roulette.java
+++ src/nanaitabeRur/Roulette.java
@@ -1,7 +1,5 @@
 package gui;
 
-import gui.RotateImage;
-
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
@@ -10,6 +8,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
@@ -23,20 +22,20 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class Roulette extends JPanel {
-	//5개 짜리 룰렛 경로
-	private final static String ROULETTE_FIVE = "C:\\Users\\Administrator\\Desktop\\img\\5th.jpg";
-	//6개짜리 룰렛 경로
-	private final static String ROULETTE_SIX = "C:\\Users\\Administrator\\Desktop\\img\\7th.jpg";
-	//8개 짜리 룰렛 경로
-	private final static String ROULETTE_EIGHTH = "C:\\Users\\Administrator\\Desktop\\img\\10th.jpg";
-	
-	String showRoulette = ROULETTE_FIVE; //룰렛화면, 기본은 5개짜리
-	File f;
-	BufferedImage bi;
-	public Roulette() {
-     setLayout(null);
-    
-    
+   // 5개 짜리 룰렛 경로
+   private final static String ROULETTE_FIVE = "D:\\img\\5th.jpg";
+   // 6개짜리 룰렛 경로
+   private final static String ROULETTE_SIX = "D:\\img\\6th.jpg";
+   // 8개 짜리 룰렛 경로
+   private final static String ROULETTE_EIGHTH = "D:\\img\\8th.jpg";
+
+   String showRoulette = ROULETTE_FIVE;   // 룰렛화면, 기본은 5개 짜리
+   File f;
+   BufferedImage bi;
+
+   public Roulette() {
+      setLayout(null);
+
       // 전체 패널
       JPanel pMain = new JPanel();   // 메인 패널 변수
       pMain.setSize(550, 978);
@@ -47,11 +46,11 @@ public class Roulette extends JPanel {
       JPanel pRoul = new JPanel();   // 룰렛 패널 변수
       pRoul.setSize(500, 400);
       pRoul.setPreferredSize(new Dimension(550, 400));
-     
+
       JLabel lRoul = new JLabel();   // 룰렛(이미지)을(를) 붙일 변수
       File imageRoulette = new File(ROULETTE_FIVE);
 
-      try {  //처음 시작 메인 화면
+      try {   // 처음 시작 메인 화면
          bi = ImageIO.read(imageRoulette);
          lRoul.setIcon(new ImageIcon(bi));
          pRoul.setPreferredSize(new Dimension(500, 400));
@@ -64,93 +63,118 @@ public class Roulette extends JPanel {
 
       pRoul.setBorder(BorderFactory.createTitledBorder("Roulette"));
 
-      // 버튼
-      JPanel pBtn = new JPanel();   // 버튼 패널 
+      // 룰렛 레이어
+      JPanel pBtn = new JPanel(); // 버튼 패널
       pBtn.setLayout(new BoxLayout(pBtn, BoxLayout.Y_AXIS));
       pBtn.setPreferredSize(new Dimension(550, 100));
 
-      JPanel pbtn1 = new JPanel();   // 메인 버튼, 누르면 회전
+      // 룰렛 버튼
+      JPanel pbtn1 = new JPanel(); // 메인 버튼, 누르면 회전
       JButton sBtn = new JButton("회전");
       sBtn.setPreferredSize(new Dimension(150, 60));
+
+      // 룰렛 돌리 때 이용 할 쓰레드
+      class myThread extends Thread {
+         public void run() {
+            Random r = new Random();
+            int time = Math.abs(r.nextInt()) % 1000;
+
+            JPanel panel = new JPanel();
+            JLabel label = new JLabel();
+
+            
+               for (int i = 0; i <= time; i += 3) { // i는 돌아가는 속도
+                  try {
+                  BufferedImage oldImage = ImageIO.read(new FileInputStream(showRoulette));
+                  BufferedImage newImage = new BufferedImage(oldImage.getHeight(), oldImage.getWidth(),
+                        oldImage.getType());
+                  Graphics2D graphics = (Graphics2D) newImage.getGraphics();
+                  graphics.rotate(Math.toRadians(i), newImage.getWidth() / 2, newImage.getHeight() / 2);
+                  graphics.translate((newImage.getWidth() - oldImage.getWidth()) / 2,
+                        (newImage.getHeight() - oldImage.getHeight()) / 2);
+                  graphics.drawImage(oldImage, 0, 0, oldImage.getWidth(), oldImage.getHeight(), null);
+                  ImageIcon icon = new ImageIcon(newImage);
+                  lRoul.setIcon(icon);
+
+                  } catch (Exception e) {
+                     // TODO: handle exception
+                     e.printStackTrace();
+                  }
+               }
+         }
+      }
+
+      // 회전 버튼 이벤트 처리
+      class ActionRotate implements ActionListener {
+
+         @Override
+         public void actionPerformed(ActionEvent arg0) {
+            if (arg0.getSource() == sBtn) {
+               // 사진이 회전하게 만들어 주세요..
+               Thread t = new myThread();
+               t.start();
+            }
+
+         }
+      }
+      
+      sBtn.addActionListener(new ActionRotate());
+
       pbtn1.add(sBtn);
       pBtn.add(pbtn1);
 
-      // 버튼(개수 버튼 1)
+      // 버튼 패널
+      JPanel otherP = new JPanel();
 
-      JPanel otherP = new JPanel();   // 5분할 된 룰렛
-      JButton btn1 = new JButton("5개");
+      // 버튼(개수 버튼 1)
+      JButton btn1 = new JButton("5개");   // 5분할 된 룰렛
       btn1.setPreferredSize(new Dimension(100, 60));
       otherP.add(btn1);
 
       // 버튼(개수 버튼 2)
-      JButton btn2 = new JButton("7개");   // 7분할 된 룰렛
+      JButton btn2 = new JButton("6개");   // 6분할 된 룰렛
       btn2.setPreferredSize(new Dimension(100, 60));
       otherP.add(btn2);
- 
-     // 버튼(개수 버튼 3)
-      JButton btn3 = new JButton("10개");   // 10분할 된 룰렛
+
+      // 버튼(개수 버튼 3)
+      JButton btn3 = new JButton("8개");   // 8분할 된 룰렛
       btn3.setPreferredSize(new Dimension(100, 60));
       otherP.add(btn3);
       pBtn.add(otherP);
 
       pBtn.setBorder(BorderFactory.createTitledBorder("메뉴"));
-      
-      //회전 버튼 이벤트 처리
-      class ActionRotate implements ActionListener{
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			if(arg0.getSource() ==sBtn) {
-				//int time = (int)(Math.random()*1000);
-				 for(int i=0; i<=180; i+=1) {  //i는 돌아가는 속도   
-					  // BufferedImage oldImage = ImageIO.read(new FileInputStream(showRoulette));
-					   BufferedImage newImage = new BufferedImage(bi.getHeight(), bi.getWidth(), bi.getType());
-					   Graphics2D graphics = (Graphics2D) newImage.getGraphics();
-					   graphics.rotate(Math.toRadians(i), newImage.getWidth() / 2, newImage.getHeight() / 2);
-					   graphics.translate((newImage.getWidth() - bi.getWidth()) / 2, (newImage.getHeight() - bi.getHeight()) / 2);
-					   graphics.drawImage(bi, 0, 0, bi.getWidth(), bi.getHeight(), null);			   
-					   ImageIcon icon = new ImageIcon(newImage);
-					   lRoul.setIcon(icon);;
-					   pBtn.add(pbtn1);
-					   setVisible(true);
-					   System.gc();
-}
-			}
-			
-		}
-    	  
-      }
+
       // 개수 버튼에 따른 이벤트 처리
       class Action1 implements ActionListener {
 
          @Override
          public void actionPerformed(ActionEvent arg0) {
 
-            if (arg0.getSource() == btn1) {
-              //5 버튼
-            	showRoulette = ROULETTE_FIVE;
+            if (arg0.getSource() == btn1) {      // 5 룰렛
+               showRoulette = ROULETTE_FIVE;
                try {
-            	  f = new File(ROULETTE_FIVE);
+                  f = new File(ROULETTE_FIVE);
                   bi = ImageIO.read(f);
                   lRoul.setIcon(new ImageIcon(bi));
                   pRoul.setPreferredSize(new Dimension(500, 400));
                } catch (IOException e) {
                   e.printStackTrace();
                }
-            } else if (arg0.getSource() == btn2) {
-              showRoulette = ROULETTE_SIX;
+            } else if (arg0.getSource() == btn2) {   // 6 룰렛
+               showRoulette = ROULETTE_SIX;
                try {
 
-            	  f = new File(ROULETTE_SIX);  //6 사진 경로
+                  f = new File(ROULETTE_SIX);
                   bi = ImageIO.read(f);
                   lRoul.setIcon(new ImageIcon(bi));
                   pRoul.setPreferredSize(new Dimension(500, 400));
                } catch (IOException e) {
-            	   e.printStackTrace();
+                  e.printStackTrace();
                }
-            } else if (arg0.getSource() == btn3) {			
-              showRoulette = ROULETTE_EIGHTH;
+            } else if (arg0.getSource() == btn3) {   // 8 룰렛
+               showRoulette = ROULETTE_EIGHTH;
                try {
-            	  f = new File(ROULETTE_EIGHTH); //8사진 경로
+                  f = new File(ROULETTE_EIGHTH);
                   bi = ImageIO.read(f);
                   lRoul.setIcon(new ImageIcon(bi));
                   pRoul.setPreferredSize(new Dimension(500, 400));
@@ -160,7 +184,6 @@ public class Roulette extends JPanel {
             }
          }
       }
-      sBtn.addActionListener(new ActionRotate());
       btn1.addActionListener(new Action1());
       btn2.addActionListener(new Action1());
       btn3.addActionListener(new Action1());
@@ -175,16 +198,16 @@ public class Roulette extends JPanel {
       JTextField tField = new JTextField(40);      // 텍스트 필드. 입력 부분
       JButton tBtn = new JButton("입력");         // 입력 후 버튼 누를시 에리어에 추가
       JTextArea tArea = new JTextArea(11, 48);   // 내용을 출력 받을 부분
-      JScrollPane sPane = new JScrollPane(tArea);   // 스크롤을 추가
+      JScrollPane sPane = new JScrollPane(tArea); // 스크롤을 추가
 
       // 글 입력에 따른 이벤트 처리
       class Action2 implements ActionListener {
-    	  int actionCnt = 0;
+         int actionCnt = 0;
 
          @Override
 
          public void actionPerformed(ActionEvent e) {
-         if (e.getSource() == tBtn || e.getSource() == tField) {
+            if (e.getSource() == tBtn || e.getSource() == tField) {
                String name = tField.getText();
                actionCnt++;
                tArea.append(actionCnt + "번째 메뉴 : " + name + "\n");
@@ -196,15 +219,13 @@ public class Roulette extends JPanel {
 
       tBtn.addActionListener(new Action2());      // 버튼 누를시 이벤트 처리
       tField.addActionListener(new Action2());   // 텍스트 바에서 엔터 누를 시 이벤트 처리
- 
+
       pText.setBorder(BorderFactory.createTitledBorder("메뉴"));
 
       pText.add(tField);
       pText.add(tBtn);
       pText.add(sPane);
       pMain.add(pText);
-
- 
 
       add(pMain);
       setVisible(true);
