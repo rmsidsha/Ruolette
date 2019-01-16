96bb919e40a54b4bf3825ac29f612cd2f20b8782
rmsidsha <43736727+rmsidsha@users.noreply.github.com>
2019-01-16 18:56:10 +0900

    Update Roulette.java

--- src/nanaitabeRur/Roulette.java
+++ src/nanaitabeRur/Roulette.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
@@ -22,16 +23,20 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class Roulette extends JPanel {
-   // 5개 짜리 룰렛 경로
-   private final static String ROULETTE_FIVE = "D:\\img\\5th.jpg";
-   // 6개짜리 룰렛 경로
-   private final static String ROULETTE_SIX = "D:\\img\\6th.jpg";
-   // 8개 짜리 룰렛 경로
-   private final static String ROULETTE_EIGHTH = "D:\\img\\8th.jpg";
+// 5개 짜리 룰렛 경로
+   private final static String ROULETTE_FIVE = "D:\\룰렛 사진 (1)\\5번.png";
+// 6개짜리 룰렛 경로
+   private final static String ROULETTE_SIX = "D:\\룰렛 사진 (1)\\6번 룰렛.png";
+// 8개 짜리 룰렛 경로
+   private final static String ROULETTE_EIGHTH = "D:\\룰렛 사진 (1)\\8번 룰렛.png";
 
    String showRoulette = ROULETTE_FIVE;   // 룰렛화면, 기본은 5개 짜리
+// 초기 설정
+   int settingNumber = 5;
+
    File f;
    BufferedImage bi;
+   //public static boolean rouletteStop = true;   //룰렛판 변경 시 룰렛을 멈춤
 
    public Roulette() {
       setLayout(null);
@@ -49,6 +54,16 @@ public class Roulette extends JPanel {
 
       JLabel lRoul = new JLabel();   // 룰렛(이미지)을(를) 붙일 변수
       File imageRoulette = new File(ROULETTE_FIVE);
+      // 텍스트 패널
+      JPanel pText = new JPanel();   // 텍스트 패널
+      pText.setSize(500, 250);
+      pText.setPreferredSize(new Dimension(550, 250));
+
+      JTextField tField = new JTextField(40);      // 텍스트 필드. 입력 부분
+      JButton tBtn = new JButton("입력");         // 입력 후 버튼 누를시 에리어에 추가
+      JTextArea tArea = new JTextArea(11, 48);   // 내용을 출력 받을 부분
+      JScrollPane sPane = new JScrollPane(tArea); // 스크롤을 추가
+
 
       try {   // 처음 시작 메인 화면
          bi = ImageIO.read(imageRoulette);
@@ -77,13 +92,19 @@ public class Roulette extends JPanel {
       class myThread extends Thread {
          public void run() {
             Random r = new Random();
-            int time = Math.abs(r.nextInt()) % 1000;
-
+            int time = Math.abs(r.nextInt()) % 10000;
+            int count =0;
             JPanel panel = new JPanel();
             JLabel label = new JLabel();
-
+            int rotation = time%360;
+            tArea.append(time+" "+rotation+"\n");
             
-               for (int i = 0; i <= time; i += 3) { // i는 돌아가는 속도
+            
+            
+			boolean ruletteStop = true;
+			while(ruletteStop ) {
+            
+               for (int i = 0; i <= time; i+=10) { // i는 돌아가는 속도
                   try {
                   BufferedImage oldImage = ImageIO.read(new FileInputStream(showRoulette));
                   BufferedImage newImage = new BufferedImage(oldImage.getHeight(), oldImage.getWidth(),
@@ -95,12 +116,97 @@ public class Roulette extends JPanel {
                   graphics.drawImage(oldImage, 0, 0, oldImage.getWidth(), oldImage.getHeight(), null);
                   ImageIcon icon = new ImageIcon(newImage);
                   lRoul.setIcon(icon);
+                  sleep(50);
 
                   } catch (Exception e) {
                      // TODO: handle exception
                      e.printStackTrace();
                   }
+                  //System.gc();
                }
+               
+             //회전판 5
+               if(rotation>=0 && rotation <= 72){  // 1번의 경우
+            	   tArea.append("1번이 당첨되었습니다!\n");
+            	   tArea.setCaretPosition(tArea.getDocument().getLength());
+            	   tField.setText("");
+               }
+               else if(rotation>=73 && rotation<=144) { //2번의 경우
+            	   tArea.append("5번이 당첨되었습니다!\n");
+            	   tArea.setForeground(Color.RED);
+            	   tArea.setCaretPosition(tArea.getDocument().getLength());
+            	   tField.setText("");
+               }
+               else if(rotation>=145 && rotation<=216) {  //3번의 경우
+            	   tArea.append("4번이 당첨되었습니다!\n");
+            	   tArea.setForeground(Color.RED);
+            	   tArea.setCaretPosition(tArea.getDocument().getLength());
+            	   tField.setText("");
+               }
+               else if(rotation>=217 && rotation<=288) {  //4번의 경우
+            	   tArea.append("3번이 당첨되었습니다!\n");
+            	   tArea.setForeground(Color.RED);
+            	   tArea.setCaretPosition(tArea.getDocument().getLength());
+            	   tField.setText("");
+               }
+               else if(rotation>=289 && rotation <=360) {  //5번의 경우
+            	   tArea.append("2번이 당첨되었습니다!\n");
+            	   tArea.setForeground(Color.RED);
+            	   tArea.setCaretPosition(tArea.getDocument().getLength());
+            	   tField.setText("");
+               }
+
+               //회전판 6
+               if(rotation>=0 && rotation <= 60)  // 1번의 경우
+               {}
+               else if(rotation>=61 && rotation<=120) { //2번의 경우
+
+               }
+               else if(rotation>=121 && rotation<=180) {  //3번의 경우
+
+               }
+               else if(rotation>=181 && rotation<=240) {  //4번의 경우
+
+               }
+               else if(rotation>=241 && rotation <=300) {  //5번의 경우
+
+               }
+               else if(rotation>=301 && rotation<=360) {  //6번의 경우
+
+               }
+
+               //회전판 8
+               if(rotation>=0 && rotation <= 45)  // 1번의 경우
+               {}
+               else if(rotation>=46 && rotation<=90) { //2번의 경우
+
+               }
+               else if(rotation>=91 && rotation<=135) {  //3번의 경우
+
+               }
+               else if(rotation>=136 && rotation<=180) {  //4번의 경우
+
+               }
+               else if(rotation>=181 && rotation <=225) {  //5번의 경우
+
+               }
+               else if(rotation>=226 && rotation<=270) {  //6번의 경우
+
+               }
+               else if(rotation>=271 && rotation<=315) {  //7번의 경우
+
+               }
+               else if(rotation>=316 && rotation<=360) {  //8번의 경우
+
+               }
+         }
+              /* int radi = 360/settingNumber;
+               for(int i = 0 ; i < settingNumber ; i++) {
+                  if((rotation >= radi * i) && (rotation < (i + 1))) {
+                     tArea.append(i + "번이 당첨되었습니다!\n");
+                     tArea.setCaretPosition(tArea.getDocument().getLength());
+                  }
+               }*/
          }
       }
 
@@ -152,6 +258,8 @@ public class Roulette extends JPanel {
 
             if (arg0.getSource() == btn1) {      // 5 룰렛
                showRoulette = ROULETTE_FIVE;
+               settingNumber = 5;
+               
                try {
                   f = new File(ROULETTE_FIVE);
                   bi = ImageIO.read(f);
@@ -162,6 +270,7 @@ public class Roulette extends JPanel {
                }
             } else if (arg0.getSource() == btn2) {   // 6 룰렛
                showRoulette = ROULETTE_SIX;
+               settingNumber = 6;
                try {
 
                   f = new File(ROULETTE_SIX);
@@ -173,6 +282,7 @@ public class Roulette extends JPanel {
                }
             } else if (arg0.getSource() == btn3) {   // 8 룰렛
                showRoulette = ROULETTE_EIGHTH;
+               settingNumber = 8;
                try {
                   f = new File(ROULETTE_EIGHTH);
                   bi = ImageIO.read(f);
@@ -190,7 +300,7 @@ public class Roulette extends JPanel {
 
       pMain.add(pBtn);
 
-      // 텍스트 패널
+     /* // 텍스트 패널
       JPanel pText = new JPanel();   // 텍스트 패널
       pText.setSize(500, 250);
       pText.setPreferredSize(new Dimension(550, 250));
@@ -199,7 +309,7 @@ public class Roulette extends JPanel {
       JButton tBtn = new JButton("입력");         // 입력 후 버튼 누를시 에리어에 추가
       JTextArea tArea = new JTextArea(11, 48);   // 내용을 출력 받을 부분
       JScrollPane sPane = new JScrollPane(tArea); // 스크롤을 추가
-
+*/
       // 글 입력에 따른 이벤트 처리
       class Action2 implements ActionListener {
          int actionCnt = 0;
@@ -218,7 +328,7 @@ public class Roulette extends JPanel {
       }
 
       tBtn.addActionListener(new Action2());      // 버튼 누를시 이벤트 처리
-      tField.addActionListener(new Action2());   // 텍스트 바에서 엔터 누를 시 이벤트 처리
+     // tField.addActionListener(new Action2());   // 텍스트 바에서 엔터 누를 시 이벤트 처리
 
       pText.setBorder(BorderFactory.createTitledBorder("메뉴"));
 
